3e794b6e7820e9a26e46cc158ccc48de0c636a9b
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-11-13 12:42:34 +0300

    init proj

---
+++ src/main/java/ru/nikituz/axioloadproj/AxioLoadProjApplication.java
@@ -0,0 +1,13 @@
+package ru.nikituz.axioloadproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxioLoadProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxioLoadProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nikituz/axioloadproj/AxioLoadProjApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikituz.axioloadproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxioLoadProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
