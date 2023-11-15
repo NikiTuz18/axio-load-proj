c22e60b37b19a4f05a38dc68d8165fccc388b88d
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-11-15 17:54:14 +0300

    fix DataSourceProperties

--- rest/src/main/java/ru/nikituz/rest/configurations/properties/DataSourceProperties.java
+++ rest/src/main/java/ru/nikituz/rest/configurations/properties/DataSourceProperties.java
@@ -1,12 +1,14 @@
 package ru.nikituz.rest.configurations.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "spring.datasource")
 @Getter
+@Setter
 public class DataSourceProperties {
     private String driverClassName;
     private String url;
