2b112d8a4c6110b88c55510a722cbebe7f0017cd
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-11-22 10:52:05 +0300

    fix Date type and add needed annotation of xml.bind in models

--- rest/src/main/java/ru/nikituz/rest/dto/PersonDto.java
+++ rest/src/main/java/ru/nikituz/rest/dto/PersonDto.java
@@ -2,10 +2,16 @@ package ru.nikituz.rest.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import ru.nikituz.rest.models.enums.Gender;
 
-import java.sql.Date;
+import java.util.Date;
 
 import static ru.nikituz.rest.utils.validation.ValidationUtil.BIRTH_DATE_NULL_ERROR_MESSAGE;
 import static ru.nikituz.rest.utils.validation.ValidationUtil.DOCUMENT_NULL_ERROR_MESSAGE;
@@ -16,23 +22,32 @@ import static ru.nikituz.rest.utils.validation.ValidationUtil.PATRONYMIC_ERROR_M
 import static ru.nikituz.rest.utils.validation.ValidationUtil.SURNAME_ERROR_MESSAGE;
 
 @Data
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PersonDto {
 
     @Pattern(regexp = NSP_REGEXP, message = NAME_ERROR_MESSAGE)
+    @XmlAttribute(name = "name")
     private String name;
 
     @Pattern(regexp = NSP_REGEXP, message = SURNAME_ERROR_MESSAGE)
+    @XmlAttribute(name = "surname")
     private String surname;
 
     @Pattern(regexp = NSP_REGEXP, message = PATRONYMIC_ERROR_MESSAGE)
+    @XmlAttribute(name = "patronymic")
     private String patronymic;
 
     @NotNull(message = BIRTH_DATE_NULL_ERROR_MESSAGE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @XmlAttribute(name = "birthDate")
     private Date birthDate;
 
     @NotNull(message = GENDER_NULL_ERROR_MESSAGE)
+    @XmlAttribute(name = "gender")
     private Gender gender;
 
     @NotNull(message = DOCUMENT_NULL_ERROR_MESSAGE)
+    @XmlElement(name = "document")
     private DocumentDto document;
 }
