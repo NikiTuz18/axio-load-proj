ee10e84d8fbd6a0a2bd349994203b5cd3bdf2a7c
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-11-22 13:09:44 +0300

    add UtilityClass annotation

--- soap/src/main/java/ru/nikituz/soap/util/UriUtil.java
+++ soap/src/main/java/ru/nikituz/soap/util/UriUtil.java
@@ -1,5 +1,8 @@
 package ru.nikituz.soap.util;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class UriUtil {
 
     public static final String NAMESPACE_URI = "https://nikituz.ru/soap/";
