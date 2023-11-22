28b056d72d79c5090b4602e287b5d8e38e0e8145
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-11-22 15:44:24 +0300

    fix ExceptionHandler

--- rest/src/main/java/ru/nikituz/rest/controllers/PersonController.java
+++ rest/src/main/java/ru/nikituz/rest/controllers/PersonController.java
@@ -3,8 +3,10 @@ package ru.nikituz.rest.controllers;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.nikituz.rest.dto.PersonDto;
 import ru.nikituz.rest.services.PersonService;
+import ru.nikituz.rest.utils.exceptions.PersonCreateException;
+import ru.nikituz.rest.utils.response.ErrorPersonResponseTransfer;
 
 @RestController
 @RequestMapping("/rest/persons")
@@ -26,5 +30,15 @@ public class PersonController {
     public ResponseEntity<String> add(@RequestBody @Valid PersonDto personDto, BindingResult bindingResult){
         log.info("Обращение к REST-сервису..");
         return personService.responseFromPost(personDto, bindingResult);
+    }
+
+
+    @ExceptionHandler
+    private ResponseEntity<ErrorPersonResponseTransfer> handlePersonException(PersonCreateException exception){
+        log.error("Ошибка конвертации при вызове SOAP-модуля:" + exception.getMessage());
+        return new ResponseEntity<>(
+                new ErrorPersonResponseTransfer(exception.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
--- rest/src/main/java/ru/nikituz/rest/services/impl/PersonServiceImpl.java
+++ rest/src/main/java/ru/nikituz/rest/services/impl/PersonServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.nikituz.rest.client.ConvertedXmlClient;
 import ru.nikituz.rest.dto.PersonDto;
 import ru.nikituz.rest.models.Person;
@@ -22,7 +21,6 @@ import ru.nikituz.rest.repositories.PersonRepository;
 import ru.nikituz.rest.services.PersonService;
 import ru.nikituz.rest.utils.exceptions.PersonCreateException;
 import ru.nikituz.rest.utils.mappers.PersonMapper;
-import ru.nikituz.rest.utils.response.ErrorPersonResponseTransfer;
 import ru.nikituz.wsdl.GetConvertedXmlResponse;
 
 import java.io.StringReader;
@@ -47,7 +45,7 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     @Transactional
-    public ResponseEntity<String> responseFromPost(PersonDto personDto, BindingResult bindingResult) {
+    public ResponseEntity<String> responseFromPost(PersonDto personDto, BindingResult bindingResult) throws PersonCreateException {
         if(bindingResult.hasErrors()){
             StringBuilder errorMessage = new StringBuilder();
             List<FieldError> errors = bindingResult.getFieldErrors();
@@ -71,7 +69,7 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     @Transactional
-    public String sendToSoap(PersonDto personDto){
+    public String sendToSoap(PersonDto personDto) throws PersonCreateException{
         try {
             String xmlText = parseToXml(personDto);
             GetConvertedXmlResponse response = convertedXmlClient.getConvertedXml(xmlText);
@@ -85,15 +83,6 @@ public class PersonServiceImpl implements PersonService {
         } catch (JsonProcessingException | JAXBException e) {
             throw new PersonCreateException(e.getMessage());
         }
-    }
-
-    @ExceptionHandler
-    private ResponseEntity<ErrorPersonResponseTransfer> handlePersonException(PersonCreateException exception){
-        log.error("Ошибка конвертации при вызове SOAP-модуля:" + exception.getMessage());
-        return new ResponseEntity<>(
-                new ErrorPersonResponseTransfer(exception.getMessage()),
-                HttpStatus.BAD_REQUEST
-        );
     }
 
     private String parseToXml(PersonDto personDto) throws JsonProcessingException {
